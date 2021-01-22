84e702d720b612355e95d669dc93189feb2c58ce
demodeos <workinweb2014@ya.ru>
2021-01-22 23:44:20 +0300

    replace methods for optimal perfomance && usability

--- src/Interfaces/RouterInterface.php
+++ src/Interfaces/RouterInterface.php
@@ -7,14 +7,15 @@ namespace Devcompru\Interfaces;
 
 interface RouterInterface
 {
-    public static function addRoutes(array $routes): bool;
+    public static function addRoutes(array $routes, bool $clean=false): bool;
     public static function addRoute(string $method, string $pattern, array $route): bool;
 
     public static function getRoutes(): array;
-    public static function getRoute(): array;
+    public static function getRoute(): array|bool;
 
     public static function matchRoute(string $uri):array;
 
+    public static function getUri(): string;
 
 
 
--- src/Router.php
+++ src/Router.php
@@ -7,19 +7,39 @@ use Devcompru\Interfaces\RouterInterface;
 
 class Router implements RouterInterface
 {
+    const L_SEPARATOR = '<';
+    const R_SEPARATOR = '>';
+
     private static array $routes = [];
-    private static array $route;
+    private static array $route = [];
+    private static array $params;
 
     private function __construct(){}
-    public static function addRoutes(array $routes): bool
+    public static function addRoutes(array $routes, bool $clean=false): bool
     {
-        self::$routes[] = $routes;
+
+        $uri = self::getUri();
+
+        $match = false;
+
+        foreach ($routes as $route) {
+         self::addRoute($route['method'], $route['pattern'], $route['route']);
+            $match = self::getCurrentRoute($uri, $route);
+            if($match)
+                break;
+        }
+
         return true;
     }
 
     public static function addRoute(string $method, string $pattern, array $route): bool
     {
-        // TODO: Implement addRoute() method.
+        self::$routes[$method][] = [
+            'pattern'=>$pattern,
+            'route'=>$route
+        ];
+
+        return true;
     }
 
     public static function getRoutes(): array
@@ -27,13 +47,56 @@ class Router implements RouterInterface
         return self::$routes;
     }
 
-    public static function getRoute(): array
+    public static function getRoute(): array|bool
     {
-        // TODO: Implement getRoute() method.
+        return self::$route;
     }
 
     public static function matchRoute(string $uri): array
     {
         // TODO: Implement matchRoute() method.
     }
+    public static function getUri(): string
+    {
+        return isset($_SERVER['REQUEST_URI'])?explode('?', $_SERVER['REQUEST_URI'])[0]:'/';
+    }
+    public static function getCurrentRoute(string $uri, array $route): array|bool
+    {
+        $pattern = $route['pattern'];
+
+        $preg_pattern = "/([\/])|([:])/";
+
+        $result = preg_split($preg_pattern, $pattern);
+        $uri_pattern = [];
+        $uri_params = [];
+        foreach ($result as $key=>$match)
+        {
+            if($match[0]!==self::L_SEPARATOR)
+            {
+                $uri_pattern[] = rtrim($match, self::R_SEPARATOR);
+            }
+            else
+            {
+                $uri_params[trim($match, self::L_SEPARATOR)] = $key;
+            }
+        }
+
+        $search_pattern = '/\/'.implode('\/', $uri_pattern). '/si';
+        if($data = preg_match($search_pattern, $uri, $matches))
+        {
+            print_r(['search'=>$search_pattern, 'uri'=>$uri, 'match'=>$matches]);
+            echo "<hr>";
+            exit;
+
+        }
+
+
+        echo "<pre>";
+       // print_r($data);
+        return false;
+    }
+
+
+
+
 }
