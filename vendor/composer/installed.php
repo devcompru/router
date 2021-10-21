3736156a44655664ebc34fefe9923180eb6dea38
Dmitry <workinweb2014@ya.ru>
2021-10-21 13:27:48 +0300

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0.x-dev',
+        'version' => '1.0.0.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5ed8bd773212a8b6a72fb26fc30da596bf7dcb94',
+        'name' => 'devcompru/router',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devcompru/router' => array(
+            'pretty_version' => '1.0.0.x-dev',
+            'version' => '1.0.0.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5ed8bd773212a8b6a72fb26fc30da596bf7dcb94',
+            'dev_requirement' => false,
+        ),
+    ),
+);
